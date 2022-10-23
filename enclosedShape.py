33f3b906f1f263d9e540ad92fdc5832d52b5fd97
Wav3F0rm <tjmoggie@student.christscollege.com>
2022-10-23 13:29:11 +1300

    plot rectangle counter clockwise

--- enclosedShape.py
+++ enclosedShape.py
@@ -1,8 +1,10 @@
+from matplotlib import markers
+from matplotlib.lines import lineStyles
 import matplotlib.pyplot as plt
 import numpy as np
 
-xCoordinates = np.array([0, 6])
-yCoordinates = np.array([0, 10])
+xCoordinates = np.array([1, 2, 3, 4, 4, 4, 3, 2, 1, 1])
+yCoordinates = np.array([1, 1, 1, 1, 2, 3, 3, 3, 3, 2])
 
 plt.plot(xCoordinates, yCoordinates)
 plt.show()
