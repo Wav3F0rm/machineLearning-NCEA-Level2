556852de6cec0eb70f38cb94d09c119f86212caf
Wav3F0rm <tjmoggie@student.christscollege.com>
2022-10-23 21:20:08 +1300

    Working logic for unsafe regions finished using matplotlib

--- enclosedShape.py
+++ enclosedShape.py
@@ -1,10 +1,46 @@
-from matplotlib import markers
-from matplotlib.lines import lineStyles
 import matplotlib.pyplot as plt
-import numpy as np
+from collections import namedtuple
 
-xCoordinates = np.array([1, 2, 3, 4, 4, 4, 3, 2, 1, 1])
-yCoordinates = np.array([1, 1, 1, 1, 2, 3, 3, 3, 3, 2])
+Point = namedtuple('Point', 'x, y')
 
-plt.plot(xCoordinates, yCoordinates)
+snake = [Point(x=1,y=5), Point(x=1,y=4), Point(x=1,y=3), Point(x=1,y=2), Point(x=1,y=1), Point(x=2,y=1),
+            Point(x=3,y=1), Point(x=4,y=1), Point(x=4,y=2), Point(x=4,y=3), Point(x=3,y=3)]
+print(snake)
+
+xcoordinates = []
+ycoordinates = []
+for i in snake:
+    xcoordinates.append(i.x)
+    ycoordinates.append(i.y)
+
+checkpoints = []
+for i in snake:
+    checkpoints.append([i.x-1, i.y])
+    checkpoints.append([i.x+1, i.y])
+    checkpoints.append([i.x, i.y-1])
+    checkpoints.append([i.x, i.y+1])
+print(checkpoints)
+
+xcheckpoints = []
+ycheckpoints = []
+for i in checkpoints:
+    xcheckpoints.append(i[0])
+    ycheckpoints.append(i[1])
+
+unsafe = []
+for i in checkpoints:
+    if checkpoints.count(i) > 1:
+        unsafe.append(i)
+print(unsafe)
+
+xunsafe = []
+yunsafe = []
+for i in unsafe:
+    xunsafe.append(i[0])
+    yunsafe.append(i[1])
+
+
+plt.plot(xcoordinates, ycoordinates, color='green', linewidth=7)
+plt.scatter(xcheckpoints, ycheckpoints, color='blue')
+plt.scatter(xunsafe, yunsafe, color='red')
 plt.show()
